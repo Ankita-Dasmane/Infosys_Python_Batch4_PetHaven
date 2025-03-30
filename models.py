28b71fd3a42a7a6f71811a07d3773f3656d02fd1
Ankita-Dasmane <ankitadasmane@gmail.com>
2025-03-31 01:23:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone, timedelta, time
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from flask import current_app as app , jsonify
-from sqlalchemy import text  # Add this import
+from sqlalchemy import text, func  # Add this func also from db
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -873,5 +873,8 @@ def fetch_recent_bookings():
             final_data = []
 
         return final_data
+
+
+
 
 
